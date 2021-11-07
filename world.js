0dde719eb576b8963e08e94041295d355eb8fc2e
Patrick Witter <pwitterop123@gmail.com>
2021-11-07 17:38:17 -0500

    Completed Ex4.

--- world.js
+++ world.js
@@ -5,10 +5,15 @@ window.onload = function () {
     inputField = document.getElementById("country");
     searchBtn = document.getElementById("lookup");
     result = document.getElementById("result");
+
+
     searchBtn.addEventListener("click", function (e) {
         e.preventDefault;
         fetch(`./world.php?country=${inputField.value.trim()}&context=countries`)
             .then(res => res.text())
             .then(data => result.innerHTML = data);
-    })
+    });
+
+
+
 }
--- world.php
+++ world.php
@@ -5,15 +5,37 @@ $password = 'password123';
 $dbname = 'world';
 
 $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
-echo " Country $country";
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<table>";
+echo "<thead>
+  <tr>
+    <th>Country Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>
+</thead>";
+foreach ($results as $row){
+  //var_dump($row);
+  $name = $row['name'];
+  $continent = $row['continent'];
+  $independence_year = $row['independence_year'];
+  $head_of_state = $row['head_of_state'];
+  echo "<tbody>
+    <tr>
+      <td>$name</td>
+      <td>$continent</td>
+      <td>$independence_year</td>
+      <td>$head_of_state</td>
+    </tr>
+  </tbody>";
+}
+echo "</table>";
+
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
